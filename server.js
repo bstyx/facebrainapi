ea322d0416eb263e0fd65fc39b69ec99eeb47384
bstyx <sebastianludtke@protonmail.com>
2020-01-03 05:38:12 +0100

    updateing database

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1",
+    host: "postgresql-sinuous-90740",
     user: "postgres",
-    password: "garten24",
+    password: "",
     database: "smartbrain"
   }
 });
